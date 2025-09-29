ec16ad39cf5b8fb68bfcb655f8e40d8af6b2f964
Mandeep8252 <mandeepsingh.cs24@bmsce.ac.in>
2025-09-29 12:06:22 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class shape{
+    int radius;
+    int length;
+    int breadth;
+    int height;
+    
+    
+        Scanner input=new Scanner(System.in);
+        void choice(int c){
+            if(c==1){
+                circle();
+            }
+            else if(c==2){
+                rectangle();
+            }
+            else if(c==3){
+                trapezium();
+
+            }
+            else{
+                System.out.println("invalid input");
+            }
+        }
+        void circle(){
+            
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+public class circle {
+    
+}
